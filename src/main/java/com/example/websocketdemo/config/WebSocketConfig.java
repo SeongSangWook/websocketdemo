748e24e9cb80c0595871137e3c137537aba344e7
SSW <SSW@DESKTOP-0BK6AVP>
2019-07-18 15:55:15 +0900

    복구

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.websokectdemo.utils;
+package com.example.websocketdemo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
